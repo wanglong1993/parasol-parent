4ebbf5955257adcdbdc6cd12601bc975fdff958c
allenshen <shenyonghua@jintong.com>
2015-11-15 23:03:37 +0800

    add exception

---
+++ parasol-metadata/parasol-metadata-api/src/main/java/com/ginkgocap/parasol/metadata/exception/CodeRegionServiceException.java
@@ -0,0 +1,35 @@
+package com.ginkgocap.parasol.metadata.exception;
+
+/**
+ * 
+ * @author allenshen
+ * @date 2015年11月11日
+ * @time 下午5:50:24
+ * @Copyright Copyright©2015 www.gintong.com
+ */
+public class CodeRegionServiceException extends Exception {
+	private int errorCode = -1;
+	public CodeRegionServiceException(Throwable cause) {
+		super(cause);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7821388402674737444L;
+
+	public CodeRegionServiceException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public CodeRegionServiceException(String message) {
+		super(message);
+	}
+
+	
+	public CodeRegionServiceException(int errorCode, String message) {
+		super(message);
+		this.errorCode = errorCode;
+	}
+
+}
