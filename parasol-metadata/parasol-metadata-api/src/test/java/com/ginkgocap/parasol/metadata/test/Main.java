b60db3ba346e5629ca1348c6cc6cabe089817179
allenshen <shenyonghua@jintong.com>
2015-11-15 23:02:13 +0800

    临时

--- parasol-metadata/parasol-metadata-api/src/test/java/com/ginkgocap/parasol/metadata/test/Main.java
+++ parasol-metadata/parasol-metadata-api/src/test/java/com/ginkgocap/parasol/metadata/test/Main.java
@@ -1,9 +1,15 @@
 package com.ginkgocap.parasol.metadata.test;
 
+import com.ginkgocap.parasol.metadata.type.CodeRegionType;
+
 public class Main {
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		String a = Long.toString(9999, 36).toUpperCase();
 		System.out.println(a);
-		System.out.println(Long.valueOf("XMTM4MzE4ODAyOA==", 36));
+		
+		System.out.println(CodeRegionType.TYPE_CHINAINLAND.getValue());
 	}
 }
