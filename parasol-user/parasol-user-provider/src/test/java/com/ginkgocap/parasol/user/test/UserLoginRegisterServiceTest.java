d2fb06e0fb60da92f4beab6f95f1aa236fc18018
liurenyuan <liurenyuan@gintong.com>
2015-11-18 18:09:55 +0800

    添加测试类
    Signed-off-by: liurenyuan <liurenyuan@gintong.com>

--- parasol-user/parasol-user-provider/src/test/java/com/ginkgocap/parasol/user/test/UserLoginRegisterServiceTest.java
+++ parasol-user/parasol-user-provider/src/test/java/com/ginkgocap/parasol/user/test/UserLoginRegisterServiceTest.java
@@ -1,5 +1,35 @@
 package com.ginkgocap.parasol.user.test;
 
-public class UserLoginRegisterServiceTest {
+import javax.annotation.Resource;
 
+import junit.framework.Test;
+import junit.framework.TestResult;
+
+import com.ginkgocap.parasol.user.exception.UserServiceException;
+import com.ginkgocap.parasol.user.model.UserLoginRegister;
+import com.ginkgocap.parasol.user.service.UserLoginRegisterService;
+
+public class UserLoginRegisterServiceTest  extends TestBase implements Test  {
+
+	@Resource
+	private UserLoginRegisterService userLoginRegisterService;
+	
+	public int countTestCases() {
+		return 0;
+	}
+
+	public void run(TestResult result) {
+		
+	}
+	@org.junit.Test
+	public void testCreateUserLoginRegister(){
+		
+		UserLoginRegister userLoginRegister = new UserLoginRegister();
+		try {
+			userLoginRegisterService.createUserLoginRegister(userLoginRegister);
+		} catch (UserServiceException e) {
+			
+			e.printStackTrace();
+		}
+	}
 }
