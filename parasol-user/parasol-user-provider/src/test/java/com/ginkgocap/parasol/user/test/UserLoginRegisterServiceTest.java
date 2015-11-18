320c4ddafc143ed1519ba763a27d68c43627d7fb
liurenyuan <liurenyuan@gintong.com>
2015-11-18 18:35:42 +0800

    修改测试类 Signed-off-by: liurenyuan <liurenyuan@gintong.com>

--- parasol-user/parasol-user-provider/src/test/java/com/ginkgocap/parasol/user/test/UserLoginRegisterServiceTest.java
+++ parasol-user/parasol-user-provider/src/test/java/com/ginkgocap/parasol/user/test/UserLoginRegisterServiceTest.java
@@ -1,9 +1,13 @@
 package com.ginkgocap.parasol.user.test;
 
+import java.util.Date;
+
 import javax.annotation.Resource;
 
 import junit.framework.Test;
 import junit.framework.TestResult;
+
+import org.junit.Assert;
 
 import com.ginkgocap.parasol.user.exception.UserServiceException;
 import com.ginkgocap.parasol.user.model.UserLoginRegister;
@@ -23,10 +27,18 @@ public class UserLoginRegisterServiceTest  extends TestBase implements Test  {
 	}
 	@org.junit.Test
 	public void testCreateUserLoginRegister(){
-		
 		UserLoginRegister userLoginRegister = new UserLoginRegister();
+		userLoginRegister.setPassport("13677687632");
+		userLoginRegister.setPassword("111111");
+		byte virtual=1;
+		userLoginRegister.setVirtual(virtual);;
+		userLoginRegister.setSalt("12121111");
+		userLoginRegister.setSource("app");
+		userLoginRegister.setCtime(new Date());
+		userLoginRegister.setUtime(new Date());
 		try {
-			userLoginRegisterService.createUserLoginRegister(userLoginRegister);
+			Long id=userLoginRegisterService.createUserLoginRegister(userLoginRegister);
+			Assert.assertTrue(id !=null && id > 0l);
 		} catch (UserServiceException e) {
 			
 			e.printStackTrace();
