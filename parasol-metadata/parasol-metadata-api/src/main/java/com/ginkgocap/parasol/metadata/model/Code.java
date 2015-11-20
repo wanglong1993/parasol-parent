27eb5837ba61fa2e34e9dd7933823839aea94dcd
allenshen <shenyonghua@jintong.com>
2015-11-21 07:47:35 +0800

    临时提交

--- parasol-metadata/parasol-metadata-api/src/main/java/com/ginkgocap/parasol/metadata/model/Code.java
+++ parasol-metadata/parasol-metadata-api/src/main/java/com/ginkgocap/parasol/metadata/model/Code.java
@@ -9,6 +9,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
+@JsonFilter("com.ginkgocap.parasol.metadata.model.Code")
 @Entity
 @Table(name = "tb_code")
 public class Code implements Serializable {
