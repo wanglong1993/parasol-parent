b41f26de8569d89f8dcbe803ec7db42689469bd7
allenshen <shenyonghua@jintong.com>
2015-12-01 18:43:11 +0800

    零时提交

--- parasol-directory/parasol-directory-web/src/main/java/com/ginkgocap/parasol/directory/web/jetty/web/controller/DirectoryController.java
+++ parasol-directory/parasol-directory-web/src/main/java/com/ginkgocap/parasol/directory/web/jetty/web/controller/DirectoryController.java
@@ -60,11 +60,201 @@ public class DirectoryController extends BaseControl {
 	private static final String paramenterAppId = "appKey"; // 应用的Key
 	private static final String paramenterUserId = "userId"; // 访问的用户参数
 	private static final String paramenterRootType = "rootType"; // 查询的应用分类
-	private static final String paramenterPid = "pid"; 			// 查询的子目录
-
+	private static final String paramenterPid = "pid"; // 查询的子目录
+	private static final String paramenterName = "name"; // 目录名称
+	private static final String paramenterDirectoryId = "directoryId"; // 目录ID
+	private static final String paramenterToDirectoryId = "toDirectoryId"; // 移动目录的生活，移动那个目录下
 
 	@Autowired
 	private DirectoryService directoryService;
+
+	/**
+	 * 2.创建分类下的根目录
+	 * 
+	 * @param request
+	 * @return
+	 * @throws DirectoryServiceException
+	 * @throws CodeServiceException
+	 */
+	@RequestMapping(path = { "/directory/directory/createDirectoryRoot" }, method = { RequestMethod.GET })
+	public MappingJacksonValue createDirectoryRoot(@RequestParam(name = DirectoryController.paramenterFields, defaultValue = "") String fileds,
+			@RequestParam(name = DirectoryController.paramenterDebug, defaultValue = "") String debug,
+			@RequestParam(name = DirectoryController.paramenterAppId, required = true) Long appId,
+			@RequestParam(name = DirectoryController.paramenterUserId, required = true) Long userId,
+			@RequestParam(name = DirectoryController.paramenterName, required = true) String name,
+			@RequestParam(name = DirectoryController.paramenterRootType, required = true) long rootType) throws DirectoryServiceException {
+		MappingJacksonValue mappingJacksonValue = null;
+		try {
+			// 0.校验输入参数（框架搞定，如果业务业务搞定）
+
+			Directory directory = new Directory();
+			directory.setAppId(appId);
+			directory.setUserId(userId);
+			directory.setName(name);
+			directory.setTypeId(rootType);
+
+			Long id = directoryService.createDirectoryForRoot(rootType, directory);
+			Map<String, Long> reusltMap = new HashMap<String, Long>();
+			reusltMap.put("id", id);
+			// 2.转成框架数据
+			mappingJacksonValue = new MappingJacksonValue(reusltMap);
+			return mappingJacksonValue;
+		} catch (RpcException e) {
+			Map<String, Serializable> resultMap = new HashMap<String, Serializable>();
+			ResponseError error = processResponseError(e);
+			if (error != null) {
+				resultMap.put("error", error);
+			}
+			if (ObjectUtils.equals(debug, "all")) {
+				// if (e.getErrorCode() > 0 ) {
+				resultMap.put("__debug__", e.getMessage());
+				// }
+			}
+			mappingJacksonValue = new MappingJacksonValue(resultMap);
+			e.printStackTrace(System.err);
+			return mappingJacksonValue;
+		} catch (DirectoryServiceException e) {
+			throw e;
+		}
+	}
+
+	/**
+	 * 删除目录
+	 * 
+	 * @param debug
+	 * @param appId
+	 * @param userId
+	 * @param directoryId
+	 * @return
+	 * @throws DirectoryServiceException
+	 */
+	@RequestMapping(path = { "/directory/directory/deleteDirectory" }, method = { RequestMethod.GET, RequestMethod.DELETE })
+	public MappingJacksonValue deleteDirectoryRoot(@RequestParam(name = DirectoryController.paramenterDebug, defaultValue = "") String debug,
+			@RequestParam(name = DirectoryController.paramenterAppId, required = true) Long appId,
+			@RequestParam(name = DirectoryController.paramenterUserId, required = true) Long userId,
+			@RequestParam(name = DirectoryController.paramenterDirectoryId, required = true) Long directoryId) throws DirectoryServiceException {
+		MappingJacksonValue mappingJacksonValue = null;
+		try {
+			// 0.校验输入参数（框架搞定，如果业务业务搞定）
+
+			Boolean b = directoryService.removeDirectory(appId, userId, directoryId);
+			Map<String, Boolean> reusltMap = new HashMap<String, Boolean>();
+			reusltMap.put("success", b);
+			// 2.转成框架数据
+			mappingJacksonValue = new MappingJacksonValue(reusltMap);
+			return mappingJacksonValue;
+		} catch (RpcException e) {
+			Map<String, Serializable> resultMap = new HashMap<String, Serializable>();
+			ResponseError error = processResponseError(e);
+			if (error != null) {
+				resultMap.put("error", error);
+			}
+			if (ObjectUtils.equals(debug, "all")) {
+				// if (e.getErrorCode() > 0 ) {
+				resultMap.put("__debug__", e.getMessage());
+				// }
+			}
+			mappingJacksonValue = new MappingJacksonValue(resultMap);
+			e.printStackTrace(System.err);
+			return mappingJacksonValue;
+		} catch (DirectoryServiceException e) {
+			throw e;
+		}
+	}
+
+	/**
+	 * 更新目录
+	 * 
+	 * @param debug
+	 * @param appId
+	 * @param userId
+	 * @param directoryId
+	 * @return
+	 * @throws DirectoryServiceException
+	 */
+	@RequestMapping(path = { "/directory/directory/updateDirectory" }, method = { RequestMethod.GET })
+	public MappingJacksonValue updateDirectoryRoot(@RequestParam(name = DirectoryController.paramenterFields, defaultValue = "") String fileds,
+			@RequestParam(name = DirectoryController.paramenterDebug, defaultValue = "") String debug,
+			@RequestParam(name = DirectoryController.paramenterAppId, required = true) Long appId,
+			@RequestParam(name = DirectoryController.paramenterUserId, required = true) Long userId,
+			@RequestParam(name = DirectoryController.paramenterDirectoryId, required = true) Long directoryId,
+			@RequestParam(name = DirectoryController.paramenterName, required = true) String name) throws DirectoryServiceException {
+		MappingJacksonValue mappingJacksonValue = null;
+		try {
+
+			Directory directory = new Directory();
+			directory.setAppId(appId);
+			directory.setName(name);
+			directory.setId(directoryId);
+
+			Boolean b = directoryService.updateDirectory(appId, userId, directory);
+			Map<String, Boolean> reusltMap = new HashMap<String, Boolean>();
+			reusltMap.put("success", b);
+			// 2.转成框架数据
+			mappingJacksonValue = new MappingJacksonValue(reusltMap);
+			return mappingJacksonValue;
+		} catch (RpcException e) {
+			Map<String, Serializable> resultMap = new HashMap<String, Serializable>();
+			ResponseError error = processResponseError(e);
+			if (error != null) {
+				resultMap.put("error", error);
+			}
+			if (ObjectUtils.equals(debug, "all")) {
+				// if (e.getErrorCode() > 0 ) {
+				resultMap.put("__debug__", e.getMessage());
+				// }
+			}
+			mappingJacksonValue = new MappingJacksonValue(resultMap);
+			e.printStackTrace(System.err);
+			return mappingJacksonValue;
+		} catch (DirectoryServiceException e) {
+			throw e;
+		}
+	}
+
+	/**
+	 * 移动目录
+	 * @param debug
+	 * @param appId
+	 * @param userId
+	 * @param directoryId
+	 * @return
+	 * @throws DirectoryServiceException
+	 */
+	@RequestMapping(path = { "/directory/directory/moveDirectory" }, method = { RequestMethod.GET })
+	public MappingJacksonValue moveDirectoryRoot(@RequestParam(name = DirectoryController.paramenterFields, defaultValue = "") String fileds,
+			@RequestParam(name = DirectoryController.paramenterDebug, defaultValue = "") String debug,
+			@RequestParam(name = DirectoryController.paramenterAppId, required = true) Long appId,
+			@RequestParam(name = DirectoryController.paramenterUserId, required = true) Long userId,
+			@RequestParam(name = DirectoryController.paramenterDirectoryId, required = true) Long directoryId,
+			@RequestParam(name = DirectoryController.paramenterToDirectoryId, required = true) Long toDirectoryId) throws DirectoryServiceException {
+		MappingJacksonValue mappingJacksonValue = null;
+		try {
+
+			Boolean b = directoryService.moveDirectoryToDirectory(appId, userId, directoryId,toDirectoryId);
+			Map<String, Boolean> reusltMap = new HashMap<String, Boolean>();
+			reusltMap.put("success", b);
+			// 2.转成框架数据
+			mappingJacksonValue = new MappingJacksonValue(reusltMap);
+			return mappingJacksonValue;
+		} catch (RpcException e) {
+			Map<String, Serializable> resultMap = new HashMap<String, Serializable>();
+			ResponseError error = processResponseError(e);
+			if (error != null) {
+				resultMap.put("error", error);
+			}
+			if (ObjectUtils.equals(debug, "all")) {
+				// if (e.getErrorCode() > 0 ) {
+				resultMap.put("__debug__", e.getMessage());
+				// }
+			}
+			mappingJacksonValue = new MappingJacksonValue(resultMap);
+			e.printStackTrace(System.err);
+			return mappingJacksonValue;
+		} catch (DirectoryServiceException e) {
+			throw e;
+		}
+	}
 
 	/**
 	 * 1. （查询类）查询分类下的根目录
@@ -74,7 +264,7 @@ public class DirectoryController extends BaseControl {
 	 * @throws DirectoryServiceException
 	 * @throws CodeServiceException
 	 */
-	@RequestMapping(path = {"/directory/directory/getRootList"}, method = { RequestMethod.GET })
+	@RequestMapping(path = { "/directory/directory/getRootList" }, method = { RequestMethod.GET })
 	public MappingJacksonValue getFunctionClassList(@RequestParam(name = DirectoryController.paramenterFields, defaultValue = "") String fileds,
 			@RequestParam(name = DirectoryController.paramenterDebug, defaultValue = "") String debug,
 			@RequestParam(name = DirectoryController.paramenterAppId, required = true) Long appId,
@@ -111,8 +301,6 @@ public class DirectoryController extends BaseControl {
 		}
 	}
 
-	
-	
 	/**
 	 * 1. （查询类)查询一个子目录
 	 * 
@@ -121,7 +309,7 @@ public class DirectoryController extends BaseControl {
 	 * @throws DirectoryServiceException
 	 * @throws CodeServiceException
 	 */
-	@RequestMapping(path = {"/directory/directory/getSubList"}, method = { RequestMethod.GET })
+	@RequestMapping(path = { "/directory/directory/getSubList" }, method = { RequestMethod.GET })
 	public MappingJacksonValue getSubList(@RequestParam(name = DirectoryController.paramenterFields, defaultValue = "") String fileds,
 			@RequestParam(name = DirectoryController.paramenterDebug, defaultValue = "") String debug,
 			@RequestParam(name = DirectoryController.paramenterAppId, required = true) Long appId,
@@ -157,8 +345,7 @@ public class DirectoryController extends BaseControl {
 			throw e;
 		}
 	}
-	
-	
+
 	@Override
 	protected void processBusinessException(ResponseError error, Exception ex) {
 		if (ex instanceof DirectoryTypeServiceException) {
