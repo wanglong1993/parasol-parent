152db8993c6ea27a13fa2e7d69687b9d42a29cbd
allenshen <shenyonghua@jintong.com>
2015-12-02 11:34:33 +0800

    零时提交

--- parasol-directory/parasol-directory-web/src/main/java/com/ginkgocap/parasol/directory/web/jetty/web/error/DirectoryErrorControl.java
+++ parasol-directory/parasol-directory-web/src/main/java/com/ginkgocap/parasol/directory/web/jetty/web/error/DirectoryErrorControl.java
@@ -47,8 +47,6 @@ public class DirectoryErrorControl {
 			ex.printStackTrace(System.err);
 			responseError.setMessage(ex.getLocalizedMessage());
 			responseError.setError_subcode(getErrorCode(ex));
-
-			
 		}
 		
 		ResponseEntity<ResponseError> responseEntity = new ResponseEntity<ResponseError>(responseError, status);
