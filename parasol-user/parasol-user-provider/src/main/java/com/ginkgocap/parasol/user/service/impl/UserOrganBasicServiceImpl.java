a3df51219352299ca9904e214e6811a490170257
liurenyuan <liurenyuan@gintong.com>
2015-12-10 21:09:18 +0800

    Signed-off-by: liurenyuan <liurenyuan@gintong.com>

--- parasol-user/parasol-user-provider/src/main/java/com/ginkgocap/parasol/user/service/impl/UserOrganBasicServiceImpl.java
+++ parasol-user/parasol-user-provider/src/main/java/com/ginkgocap/parasol/user/service/impl/UserOrganBasicServiceImpl.java
@@ -7,6 +7,7 @@ import javax.annotation.Resource;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import com.ginkgocap.parasol.common.service.exception.BaseServiceException;
 import com.ginkgocap.parasol.common.service.impl.BaseService;
@@ -20,16 +21,6 @@ import com.ginkgocap.parasol.util.PinyinUtils;
 public class UserOrganBasicServiceImpl extends BaseService<UserOrganBasic> implements UserOrganBasicService  {
 	@Resource
 	private UserLoginRegisterService userLoginRegisterService;
-	private static int error_userOrganBasic_null = 1000;	
-	private static int error_userId_null = 1001;	
-	private static int error_name_null = 1004;
-	private static int error_ip_incorrect = 1008;
-	private static int error_status_incorrect = 1009;
-	private static int error_userId_not_exists_in_userlogin = 1010;	
-	private static int error_userIds_null = 1011;	
-	private static int error_userId_not_exists_in_userOrganBasic = 1015;	
-	private static int error_auth_is_error = 1016;	
-	private static int error_status_is_error = 1016;	
 	private static Logger logger = Logger.getLogger(UserOrganBasicServiceImpl.class);
 	
 	/**
@@ -39,18 +30,18 @@ public class UserOrganBasicServiceImpl extends BaseService<UserOrganBasic> imple
 	 * @throws UserOrganBasicServiceException
 	 */
 	private UserOrganBasic checkValidity(UserOrganBasic userOrganBasic,int type)throws UserOrganBasicServiceException,UserLoginRegisterServiceException {
-		if(userOrganBasic==null) throw new UserOrganBasicServiceException(error_userOrganBasic_null,"userOrganBasic can not be null.");
-		if(userOrganBasic.getUserId()<=0l) throw new UserOrganBasicServiceException(error_userId_null,"The value of userId is null or empty.");
+		if(userOrganBasic==null) throw new UserOrganBasicServiceException("userOrganBasic can not be null.");
+		if(userOrganBasic.getUserId()<=0l) throw new UserOrganBasicServiceException("The value of userId is null or empty.");
 		try {
-			if(userLoginRegisterService.getUserLoginRegister(userOrganBasic.getUserId())==null) throw new UserLoginRegisterServiceException(error_userId_not_exists_in_userlogin,"userId not exists in userLoginRegister");
+			if(userLoginRegisterService.getUserLoginRegister(userOrganBasic.getUserId())==null) throw new UserLoginRegisterServiceException("userId not exists in userLoginRegister.");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		if(type!=0)
-		if(getUserOrganBasic(userOrganBasic.getUserId())==null)throw new UserOrganBasicServiceException(error_userId_not_exists_in_userOrganBasic,"userId not exists in userOrganBasic");
-		if(StringUtils.isEmpty(userOrganBasic.getName()))throw new UserOrganBasicServiceException(error_name_null,"The value of  name is null or empty.");
-		if(StringUtils.isEmpty(userOrganBasic.getIp()))throw new UserOrganBasicServiceException(error_ip_incorrect,"The value of ip is null or empty.");
-		if(userOrganBasic.getStatus().intValue() != 0 && userOrganBasic.getStatus().intValue() != 1 && userOrganBasic.getStatus().intValue() != -1 && userOrganBasic.getStatus() !=2)throw new UserOrganBasicServiceException(error_status_incorrect,"The value of status is null or empty.");
+		if(getUserOrganBasic(userOrganBasic.getUserId())==null)throw new UserOrganBasicServiceException("userId not exists in userOrganBasic");
+		if(StringUtils.isEmpty(userOrganBasic.getName()))throw new UserOrganBasicServiceException("The value of  name is null or empty.");
+		if(StringUtils.isEmpty(userOrganBasic.getIp()))throw new UserOrganBasicServiceException("The value of ip is null or empty.");
+		if(userOrganBasic.getStatus().intValue() != 0 && userOrganBasic.getStatus().intValue() != 1 && userOrganBasic.getStatus().intValue() != -1 && userOrganBasic.getStatus() !=2)throw new UserOrganBasicServiceException("The value of status is null or empty.");
 		if(userOrganBasic.getCtime()==null) userOrganBasic.setCtime(System.currentTimeMillis());
 		if(userOrganBasic.getUtime()==null) userOrganBasic.setUtime(System.currentTimeMillis());
 		userOrganBasic.setNameFirst(StringUtils.substring(PinyinUtils.stringToHeads(userOrganBasic.getName()), 0, 1));
@@ -62,7 +53,9 @@ public class UserOrganBasicServiceImpl extends BaseService<UserOrganBasic> imple
 	@Override
 	public Long createUserOrganBasic(UserOrganBasic userOrganBasic)throws UserOrganBasicServiceException,UserLoginRegisterServiceException{
 		try {
-			return (Long) saveEntity(checkValidity(userOrganBasic,0));
+			Long id=(Long) saveEntity(checkValidity(userOrganBasic,0));
+			if(!ObjectUtils.isEmpty(id) && id>0l)return  id;
+			else throw new UserOrganBasicServiceException("createUserOrganBasic failed.");
 		} catch (BaseServiceException e) {
 			if (logger.isDebugEnabled()) {
 				e.printStackTrace(System.err);
@@ -86,7 +79,7 @@ public class UserOrganBasicServiceImpl extends BaseService<UserOrganBasic> imple
 	@Override
 	public UserOrganBasic getUserOrganBasic(Long userId) throws UserOrganBasicServiceException {
 		try {
-			if(userId==null || userId<=0l)throw new UserOrganBasicServiceException(error_userId_null,"userId is null or empty");
+			if(userId==null || userId<=0l)throw new UserOrganBasicServiceException("userId is null or empty");
 			return getEntity(userId);
 		} catch (BaseServiceException e) {
 			if (logger.isDebugEnabled()) {
@@ -99,7 +92,7 @@ public class UserOrganBasicServiceImpl extends BaseService<UserOrganBasic> imple
 	@Override
 	public List<UserOrganBasic> getUserOrganBasecList(List<Long> userIds)throws UserOrganBasicServiceException {
 		try {
-			if(userIds==null || userIds.size()==0)throw new UserOrganBasicServiceException(error_userIds_null,"userIds is null or empty");
+			if(userIds==null || userIds.size()==0)throw new UserOrganBasicServiceException("userIds is null or empty");
 			return getEntityByIds(userIds);
 		} catch (BaseServiceException e) {
 			if (logger.isDebugEnabled()) {
@@ -112,9 +105,9 @@ public class UserOrganBasicServiceImpl extends BaseService<UserOrganBasic> imple
 	@Override
 	public boolean updateAuth(Long userId, Byte auth)throws UserOrganBasicServiceException,UserLoginRegisterServiceException {
 		try {
-			if(auth.intValue()!=-1 && auth.intValue()!=0 && auth.intValue()!=1 && auth.intValue()!=2)throw new UserOrganBasicServiceException(error_auth_is_error,"auth  is error, must be -1 or 0 or 1 or 2.");
+			if(auth.intValue()!=-1 && auth.intValue()!=0 && auth.intValue()!=1 && auth.intValue()!=2)throw new UserOrganBasicServiceException("auth  is error, must be -1 or 0 or 1 or 2.");
 			UserOrganBasic userOrganBasic=getUserOrganBasic(userId);
-			if(userOrganBasic==null)throw new UserOrganBasicServiceException(error_userId_not_exists_in_userOrganBasic,"userId not exists in userOrganBasic");
+			if(userOrganBasic==null)throw new UserOrganBasicServiceException("userId not exists in userOrganBasic");
 			userOrganBasic.setAuth(auth);
 			return updateEntity(userOrganBasic);
 		} catch (BaseServiceException e) {
@@ -128,9 +121,9 @@ public class UserOrganBasicServiceImpl extends BaseService<UserOrganBasic> imple
 	@Override
 	public boolean updateStatus(Long userId, Byte status)throws UserOrganBasicServiceException,UserLoginRegisterServiceException {
 		try {
-			if(status.intValue()!=-1 && status.intValue()!=0 && status.intValue()!=1 && status.intValue()!=2)throw new UserOrganBasicServiceException(error_status_is_error,"status is error, must be -1 or 0 or 1 or 2.");
+			if(status.intValue()!=-1 && status.intValue()!=0 && status.intValue()!=1 && status.intValue()!=2)throw new UserOrganBasicServiceException("status is error, must be -1 or 0 or 1 or 2.");
 			UserOrganBasic userOrganBasic=getUserOrganBasic(userId);
-			if(userOrganBasic==null)throw new UserOrganBasicServiceException(error_userId_not_exists_in_userOrganBasic,"userId not exists in userOrganBasic");
+			if(userOrganBasic==null)throw new UserOrganBasicServiceException("userId not exists in userOrganBasic.");
 			userOrganBasic.setStatus(status);
 			return updateEntity(userOrganBasic);
 		}catch (BaseServiceException e) {
--- parasol-user/parasol-user-web/src/main/java/com/ginkgocap/parasol/user/web/jetty/web/controller/UserLoginRegisterController.java
+++ parasol-user/parasol-user-web/src/main/java/com/ginkgocap/parasol/user/web/jetty/web/controller/UserLoginRegisterController.java
@@ -6,20 +6,24 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import net.sf.json.JSONObject;
-
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.converter.json.MappingJacksonValue;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ginkgocap.parasol.user.exception.UserLoginRegisterServiceException;
+import com.ginkgocap.parasol.user.model.UserBasic;
 import com.ginkgocap.parasol.user.model.UserLoginRegister;
+import com.ginkgocap.parasol.user.model.UserLoginThird;
+import com.ginkgocap.parasol.user.model.UserOrganBasic;
+import com.ginkgocap.parasol.user.service.UserBasicService;
 import com.ginkgocap.parasol.user.service.UserLoginRegisterService;
+import com.ginkgocap.parasol.user.service.UserLoginThirdService;
+import com.ginkgocap.parasol.user.service.UserOrganBasicService;
+import com.ginkgocap.parasol.user.web.jetty.web.utils.Base64;
 
 /**
  * 用户登录注册
@@ -28,59 +32,169 @@ import com.ginkgocap.parasol.user.service.UserLoginRegisterService;
 public class UserLoginRegisterController extends BaseControl {
 	private static Logger logger = Logger.getLogger(UserLoginRegisterController.class);
 	@Autowired
+	private UserLoginThirdService userLoginThirdService;
+	@Autowired
 	private UserLoginRegisterService userLoginRegisterService;
+	@Autowired
+	private UserBasicService userBasicService;
+	@Autowired
+	private UserOrganBasicService userOrganBasicService;
 	/**
 	 * 用户注册
 	 * 
-	 * @param request
-	 * @return
+	 * @param type 1.邮箱注册,2.手机注册
+	 * @param passport 为邮箱和手机号
+	 * @param userType 1.个人用户,2.组织用户
+	 * @source Auth认正的appkey
+	 * @param 用户昵称
+	 * @param industryId 用户感兴趣的行业
 	 * @throws UserLoginRegisterServiceException
 	 */
 	@RequestMapping(path = { "/userLoginRegister/register" }, method = { RequestMethod.GET })
 	public MappingJacksonValue register(HttpServletRequest request,HttpServletResponse response
 			,@RequestParam(name = "type",required = true) int type
 			,@RequestParam(name = "passport",required = true) String passport
+			,@RequestParam(name = "password",required = true) String password
+			,@RequestParam(name = "userType",required = true) String userType
+			,@RequestParam(name = "source",required = true) String source
+			,@RequestParam(name = "name",required = true) String name
+			,@RequestParam(name = "industryId",required = true) String industryId
 			)throws UserLoginRegisterServiceException {
+		Map<String, Object> reusltMap = new HashMap<String, Object>();
+		UserLoginRegister userLoginRegister= new UserLoginRegister();
+		UserOrganBasic userOrganBasic= new UserOrganBasic();
+		UserBasic userBasic= new UserBasic();
+		UserLoginThird userLoginThird= new UserLoginThird();
+		MappingJacksonValue mappingJacksonValue=null;
+		String ip=getIpAddr(request);
+		Long userId=0l;
+		Long id=0l;
+		Long id2=0l;
 		try {
-			Map<String, Object> reusltMap = new HashMap<String, Object>();
-//				id=userLoginRegisterService.createUserLoginRegister(setUserLoginRegister(json.getString("passport"),new Byte(json.getString("virtual"))));
-//				String identifyingCode= userLoginRegisterService.sendIdentifyingCode("13677687632");
-//				UserLoginRegister ulr=userLoginRegisterService.getUserLoginRegister(3913092224516104l);
-//				ulr.getUserName();
-//				logger.info("你的短信验证码为:"+identifyingCode+",有效时间1分钟");
-		        Map<String, Object> map = new HashMap<String, Object>();
-		        map.put("email", "http://www.gintong.com#/verify?from=1&type=1&e=Y2Nra2t0dEAxMjYuY29t&email=MTUxMjEwMTUxMzE5MTAxPV89Y2Nra2t0dCU0MDEyNi5jb20=");
-		        map.put("acceptor",passport);
-		        map.put("imageRoot", "http://static.gintong.com/resources/images/v3/");
-				userLoginRegisterService.sendEmail(passport, type, map);
+				boolean exists=userLoginRegisterService.passportIsExist(passport);
+				if(exists){
+					if(type==1)reusltMap.put( "error", "email already exists.");
+					if(type==2)reusltMap.put( "error", "mobile already exists.");
+					reusltMap.put( "status", 0);
+					return new MappingJacksonValue(reusltMap);
+				}
+				//个人用户邮箱注册
+				if(type==1 && userType=="1"){
+					userLoginRegister.setPassport(passport);
+					byte[] bt = Base64.decode(password);
+					String salt=userLoginRegisterService.setSalt();
+					password=userLoginRegisterService.setSha256Hash(salt, new String(bt));
+					userLoginRegister.setSalt(salt);
+					userLoginRegister.setPassword(password);
+					userLoginRegister.setVirtual(new Byte(userType));
+					userLoginRegister.setIp(ip);
+					userLoginRegister.setSource(source);
+					id=userLoginRegisterService.createUserLoginRegister(userLoginRegister);
+			        Map<String, Object> map = new HashMap<String, Object>();
+			        map.put("email", "http://www.gintong.com/userLoginRegister/verification?eamil="+Base64.decode(passport.getBytes()));
+			        map.put("acceptor",passport);
+			        map.put("imageRoot", "http://static.gintong.com/resources/images/v3/");
+					if(userLoginRegisterService.sendEmail(passport, type, map)){
+						reusltMap.put( "message", "send mail success");
+						reusltMap.put( "status", 1);
+					}else{
+						reusltMap.put( "error", "send email failed.");
+						reusltMap.put( "status", 1);
+					}
+					return new MappingJacksonValue(reusltMap);
+				}
+				//组织用户邮箱注册
+				if(type==1 && userType=="2"){
+					userLoginRegister.setPassport(passport);
+					byte[] bt = Base64.decode(password);
+					String salt=userLoginRegisterService.setSalt();
+					password=userLoginRegisterService.setSha256Hash(salt, new String(bt));
+					userLoginRegister.setSalt(salt);
+					userLoginRegister.setPassword(password);
+					userLoginRegister.setVirtual(new Byte(userType));
+					userLoginRegister.setIp(ip);
+					userLoginRegister.setSource(source);
+					id=userLoginRegisterService.createUserLoginRegister(userLoginRegister);
+					userOrganBasic.setUserId(id);
+					userOrganBasic.setName(name);
+					userId=userOrganBasicService.createUserOrganBasic(userOrganBasic);
+					Map<String, Object> map = new HashMap<String, Object>();
+					map.put("email", "http://www.gintong.com/userLoginRegister/verification?eamil="+Base64.decode(passport.getBytes()));
+					map.put("acceptor",passport);
+					map.put("imageRoot", "http://static.gintong.com/resources/images/v3/");
+					if(userLoginRegisterService.sendEmail(passport, type, map)){
+						reusltMap.put( "message", "send mail success");
+						reusltMap.put( "status", 1);
+					}else{
+						reusltMap.put( "error", "send email failed.");
+						reusltMap.put( "status", 1);
+					}
+					return new MappingJacksonValue(reusltMap);
+				}
+				//设置userLoginRegister开始
+				userLoginRegister.setPassport(passport);
+				byte[] bt = Base64.decode(password);
+				String salt=userLoginRegisterService.setSalt();
+				password=userLoginRegisterService.setSha256Hash(salt, new String(bt));
+				userLoginRegister.setSalt(salt);
+				userLoginRegister.setPassword(password);
+				userLoginRegister.setVirtual(new Byte(userType));
+				userLoginRegister.setIp(ip);
+				userLoginRegister.setSource(source);
+				//设置userBasic开始
+				userBasic.setName(name);
+				userBasic.setSex(new Byte(sex));
+				userBasic.setIp(ip);
+				userBasic.setStatus(new Byte("1"));
+				//设置userLoginThird开始
+				userLoginThird= new UserLoginThird();
+				userLoginThird.setAccesstoken(accessToken);
+				userLoginThird.setOpenId(openId);
+				userLoginThird.setLoginType(Integer.parseInt(loginType));
+				userLoginThird.setIp(ip);
+				userLoginThird.setHeadPic(headPic);
+				//保存userLoginRegister开始
+				id=userLoginRegisterService.createUserLoginRegister(userLoginRegister);
+				//保存userBasic开始
+				userBasic.setUserId(id);
+				userId=userBasicService.createUserBasic(userBasic);
+				//保存userLoginThird开始
+				userLoginThird.setUserId(id);
+				id2=userLoginThirdService.saveUserLoginThird(userLoginThird);
+				
+				reusltMap.put("userLoginRegister",userLoginRegister);
+				reusltMap.put("userBasic",userBasic);
+				reusltMap.put("userLoginThird",userLoginThird);
+				reusltMap.put("status",1);
+				logger.info("第三注册绑定新用户成功,用户id:"+id);
+				return new MappingJacksonValue(reusltMap);
+				}
+				
+
 				reusltMap.put("status", 1);
+				logger.info("注册成功:"+passport);
 			return new MappingJacksonValue(reusltMap);
 		}catch (UserLoginRegisterServiceException e ){
+			logger.info("注册失败:"+passport);
 			throw e;
 		}
 	}
 	/**
-	 * 初始化登录注册用户对象
-	 * @return userLoginRegister
+	 * 获取真实IP的方法
+	 * @param request
+	 * @return
 	 */
-	public UserLoginRegister setUserLoginRegister(String passport,Byte virtual){
-		try {
-			Long ctime=System.currentTimeMillis();
-			UserLoginRegister userLoginRegister = new UserLoginRegister();
-			userLoginRegister.setPassport(passport);
-			userLoginRegister.setVirtual(virtual);;
-			String salt=userLoginRegisterService.setSalt();
-			String password=userLoginRegisterService.setSha256Hash(salt, "123456");
-			userLoginRegister.setSalt(salt);
-			userLoginRegister.setPassword(password);
-			userLoginRegister.setSource("app");
-			userLoginRegister.setIp("111.111.11.11");
-			userLoginRegister.setCtime(ctime);
-			userLoginRegister.setUtime(ctime);
-			return userLoginRegister;
-		} catch (Exception e) {
-			e.printStackTrace();
+	public String getIpAddr(HttpServletRequest request) {
+		String ip = request.getHeader("x-forwarded-for");
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("Proxy-Client-IP");
 		}
-		return null;
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("WL-Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getRemoteAddr();
+		}
+		return ip;
 	}	
 }
