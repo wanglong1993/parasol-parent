2fa93cb7ddb7ec4b1adebce78623033a64031733
allenshen <shenyonghua@jintong.com>
2015-12-26 16:00:02 +0800

    init parasol-tags project

---
+++ parasol-tags/parasol-tags-api/src/test/java/com/ginkgocap/parasol/tags/test/Main.java
@@ -0,0 +1,12 @@
+package com.ginkgocap.parasol.tags.test;
+
+
+public class Main {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String a = Long.toString(9999, 36).toUpperCase();
+		System.out.println(a);
+	}
+}
