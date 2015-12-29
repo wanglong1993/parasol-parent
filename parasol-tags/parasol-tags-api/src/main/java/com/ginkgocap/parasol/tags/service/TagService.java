85298497fce4d178f080499febc319b2cc60e96b
allenshen <shenyonghua@jintong.com>
2015-12-29 15:07:09 +0800

    零时提交

--- parasol-tags/parasol-tags-api/src/main/java/com/ginkgocap/parasol/tags/service/TagService.java
+++ parasol-tags/parasol-tags-api/src/main/java/com/ginkgocap/parasol/tags/service/TagService.java
@@ -7,6 +7,7 @@ import com.ginkgocap.parasol.tags.model.Tag;
 
 /**
  * 如果要是分库本Service用UserId
+ * 
  * @author allenshen
  * @date 2015年12月23日
  * @time 下午12:00:36
@@ -16,6 +17,7 @@ public interface TagService {
 
 	/**
 	 * 创建一个标签
+	 * 
 	 * @param userId
 	 * @param tag
 	 * @return
@@ -24,8 +26,8 @@ public interface TagService {
 	public Long createTag(Long userId, Tag tag) throws TagServiceException;
 
 	/**
-	 * 删除一个标签,
-	 * Note：要删除使用此标签的资源下边的引用
+	 * 删除一个标签, Note：要删除使用此标签的资源下边的引用
+	 * 
 	 * @param userId
 	 * @param tagId
 	 * @return
@@ -35,6 +37,7 @@ public interface TagService {
 
 	/**
 	 * 更新标签
+	 * 
 	 * @param userId
 	 * @param tag
 	 * @return
@@ -44,6 +47,7 @@ public interface TagService {
 
 	/**
 	 * 查询一个标签
+	 * 
 	 * @param userId
 	 * @param id
 	 * @return
@@ -52,7 +56,17 @@ public interface TagService {
 	public Tag getTag(Long userId, Long id) throws TagServiceException;
 
 	/**
+	 * 查询标签集合，通过ids
+	 * @param userId
+	 * @param ids
+	 * @return
+	 * @throws TagServiceException
+	 */
+	public List<Tag> getTags(Long userId, List<Long> ids) throws TagServiceException;
+
+	/**
 	 * 查找指定用户指定应用指定分类的Tag列表
+	 * 
 	 * @param userId
 	 * @param appId
 	 * @param tagType
@@ -63,6 +77,7 @@ public interface TagService {
 
 	/**
 	 * 查找指定用户指定应用指定分类的Tag列表
+	 * 
 	 * @param appId
 	 * @param userId
 	 * @param pId
