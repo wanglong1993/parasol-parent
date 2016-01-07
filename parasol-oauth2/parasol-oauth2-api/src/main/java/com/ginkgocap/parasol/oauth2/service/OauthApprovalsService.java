cfa1038a6ba1a0e8f62fbb4d84572927afd54b31
liurenyuan <liurenyuan@gintong.com>
2016-01-07 13:48:52 +0800

    Signed-off-by: liurenyuan <liurenyuan@gintong.com>

--- parasol-oauth2/parasol-oauth2-api/src/main/java/com/ginkgocap/parasol/oauth2/service/OauthApprovalsService.java
+++ parasol-oauth2/parasol-oauth2-api/src/main/java/com/ginkgocap/parasol/oauth2/service/OauthApprovalsService.java
@@ -1,11 +1,19 @@
 package com.ginkgocap.parasol.oauth2.service;
 
-import org.springframework.security.oauth2.provider.approval.ApprovalStore;
+import java.util.Collection;
+
+import com.ginkgocap.parasol.oauth2.model.OauthApprovals;
 
 /**
  * 操作用户权限信息接口定义
  * 
  */
-public interface OauthApprovalsService extends ApprovalStore {
+public interface OauthApprovalsService  {
+	
+	public boolean addApprovals(Collection<OauthApprovals> approvals);
+
+	public boolean revokeApprovals(Collection<OauthApprovals> approvals);
+
+	public Collection<OauthApprovals> getApprovals(String userId, String clientId);
 
 }
