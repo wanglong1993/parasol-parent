0870387b3aa4f8167c7ca64d070b1aeed415fb6c
liurenyuan <liurenyuan@gintong.com>
2016-01-07 14:53:50 +0800

    Signed-off-by: liurenyuan <liurenyuan@gintong.com>

--- parasol-oauth2/parasol-oauth2-provider/src/main/java/com/ginkgocap/parasol/oauth2/service/impl/OauthApprovalsServiceImpl.java
+++ parasol-oauth2/parasol-oauth2-provider/src/main/java/com/ginkgocap/parasol/oauth2/service/impl/OauthApprovalsServiceImpl.java
@@ -2,11 +2,11 @@ package com.ginkgocap.parasol.oauth2.service.impl;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 
 import org.apache.log4j.Logger;
-import org.springframework.security.oauth2.provider.approval.Approval;
 import org.springframework.security.oauth2.provider.approval.Approval.ApprovalStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
@@ -25,15 +25,17 @@ public class OauthApprovalsServiceImpl extends BaseService<OauthApprovals> imple
 	public boolean addApprovals(Collection<OauthApprovals> approvals) {
 		List<OauthApprovals> list=new ArrayList<OauthApprovals>();
 		OauthApprovals oauthApprovals=null;
-		for (Approval approval : approvals) {
-			oauthApprovals=new OauthApprovals();
-			oauthApprovals.setClientId(approval.getClientId());
-			oauthApprovals.setUserId(approval.getUserId());
-			oauthApprovals.setScope(approval.getScope());
-			oauthApprovals.setStatus_(approval.getStatus().toString());
-			oauthApprovals.setExpiresAt(approval.getExpiresAt());
-			oauthApprovals.setLastUpdatedAt(approval.getLastUpdatedAt());
-			list.add(oauthApprovals);
+		for (OauthApprovals approval : approvals) {
+			if(approval!=null){
+				oauthApprovals=new OauthApprovals();
+				oauthApprovals.setClientId(approval.getClientId());
+				oauthApprovals.setUserId(approval.getUserId());
+				oauthApprovals.setScope(approval.getScope());
+				oauthApprovals.setStatus_(approval.getStatus().toString());
+				oauthApprovals.setExpiresAt(approval.getExpiresAt());
+				oauthApprovals.setLastUpdatedAt(approval.getLastUpdatedAt());
+				list.add(oauthApprovals);
+			}
 		}
 		try {
 			List<OauthApprovals> lists= saveEntitys(list);
@@ -50,17 +52,20 @@ public class OauthApprovalsServiceImpl extends BaseService<OauthApprovals> imple
 
 	@Override
 	public boolean revokeApprovals(Collection<OauthApprovals> approvals) {
-		List<Long> ids=new ArrayList<Long>();
-		OauthApprovals oauthApprovals=null;
-		for (Approval approval : approvals) {
-			oauthApprovals=(OauthApprovals) approval;
-			ids.add(oauthApprovals.getId());
+		String userId=null;
+		String clientId=null;
+		for (OauthApprovals approval : approvals) {
+			userId=approval.getUserId();
+			clientId=approval.getClientId();
+			break;
 		}
 		try {
+			List<Long> ids= getIds(OauthApprovals_List_Id,clientId,userId);
+			if(ids==null) return false;
 			deleteEntityByIds(ids);
 			return true;
-		} catch (BaseServiceException e) {
-			e.printStackTrace();
+		} catch (BaseServiceException e1) {
+			e1.printStackTrace();
 		}
 		return false;
 	}
@@ -69,8 +74,10 @@ public class OauthApprovalsServiceImpl extends BaseService<OauthApprovals> imple
 	public Collection<OauthApprovals> getApprovals(String userId, String clientId) {
 		try {
 			List<Long> ids=getIds(OauthApprovals_List_Id,clientId,userId);
+			if(ids==null)return Collections.EMPTY_SET;
 			List<OauthApprovals> list=getEntityByIds(ids);
 			Collection<OauthApprovals> collection = new HashSet<OauthApprovals>();
+			if(list==null) return Collections.EMPTY_SET;
 			for (OauthApprovals oauthApprovals : list) {
 				oauthApprovals.setExpiresAt(oauthApprovals.getExpiresAt());
 				if(oauthApprovals.getStatus_().equals("APPROVED")){
@@ -84,6 +91,6 @@ public class OauthApprovalsServiceImpl extends BaseService<OauthApprovals> imple
 		} catch (BaseServiceException e) {
 			e.printStackTrace();
 		}
-		return null;
+		return Collections.EMPTY_SET;
 	}
 }
