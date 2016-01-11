773f644a1a988aadabb2570027bdcf0a5ce57d70
allenshen <shenyonghua@jintong.com>
2016-01-11 16:11:45 +0800

    change type

--- parasol-tags/parasol-tags-api/src/main/resources/create.sql
+++ parasol-tags/parasol-tags-api/src/main/resources/create.sql
@@ -2,7 +2,7 @@ CREATE TABLE `tb_tag` (
   `id` bigint(20) NOT NULL COMMENT '标签ID',
   `userId` bigint(20) NOT NULL COMMENT '用户ID',
   `appId` bigint(20) NOT NULL COMMENT '应用ID',
-  `tagType` int(3) DEFAULT '0' COMMENT 'tag的分类（比如是：知识、组织、人、图片）默认0',
+  `tagType` bigint(20) DEFAULT '0' COMMENT 'tag的分类（比如是：知识、组织、人、图片）默认0',
   `tagName` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '标签名称',
   PRIMARY KEY (`id`),
   UNIQUE KEY `appId_userId_tagType_tagName` (`appId`,`userId`,`tagType`,`tagName`)
@@ -14,7 +14,7 @@ CREATE TABLE `tb_tag_source` (
   `appId` bigint(20) NOT NULL COMMENT 'Source 的应用ID',
   `userId` bigint(20) NOT NULL COMMENT '创建TagSource的人',
   `sourceId` bigint(20) NOT NULL COMMENT '资源ID',
-  `sourceType` int(3) DEFAULT 0  COMMENT '资源类型 知识、人脉',
+  `sourceType` bigint(20) DEFAULT 0  COMMENT '资源类型 知识、人脉',
   `createAt` bigint(20) DEFAULT '0' COMMENT '更新时间',
   PRIMARY KEY (`id`),
   UNIQUE KEY `tagId_appId_sourceId_sourceType` (`tagId`,`appId`,`sourceId`,`sourceType`)
--- parasol-tags/parasol-tags-provider/src/test/resources/create.sql
+++ parasol-tags/parasol-tags-provider/src/test/resources/create.sql
@@ -1,46 +1,21 @@
-create database parasol_metadata;
-use parasol_metadata;
-CREATE TABLE `tb_directory_type` (
-  `id` bigint(20) NOT NULL COMMENT '主键',
-  `appId` bigint(20) NOT NULL COMMENT '应用ID',
-  `name` varchar(20) NOT NULL COMMENT '分类名字 (人脉、组织、需求、知识），应用系统自己定义的分类',
-  `updateAt` bigint(20) DEFAULT '0' COMMENT '更新时间',
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `appId_name` (`appId`,`name`) 
-) ENGINE=InnoDB AUTO_INCREMENT=1307 DEFAULT CHARSET=utf8
-
-CREATE TABLE `tb_directory` (
-  `id` bigint(20) NOT NULL COMMENT '主键',
-  `pid` bigint(20) NOT NULL COMMENT '父主键',
-  `appId` bigint(20) NOT NULL COMMENT '应用ID',
-  `userId` bigint(20) NOT NULL COMMENT '用户ID（Ower）',
-  `name` varchar(50) NOT NULL COMMENT '目录名称',
-  `nameIndex` varchar(250) DEFAULT NULL  COMMENT '拼音简评',
-  `nameIndexAll` varchar(250) DEFAULT NULL COMMENT '全拼',
-  `remark` varchar(250) DEFAULT NULL COMMENT '描述',
-  `numberCode` varchar(250) DEFAULT NULL COMMENT '保存父亲爷爷。。。',
-  `orderNo` int(4) DEFAULT '0' COMMENT '顺序',
-  `typeId` bigint(20) NOT NULL COMMENT '分类名字 (人脉、组织、需求、知识），应用系统自己定义的分类',
-  `updateAt` bigint(20) DEFAULT '0' COMMENT '更新时间',
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `appId_pid_userId_name` (`appId`, `pid`,`userId`,`name`) 
-) ENGINE=InnoDB AUTO_INCREMENT=1307 DEFAULT CHARSET=utf8
-
-
-CREATE TABLE `tb_directory_source` (
-  `id` bigint(20) NOT NULL COMMENT '主键',
-  `directoryId` bigint(20) NOT NULL COMMENT '目录ID',
-  `appId` bigint(20) NOT NULL COMMENT 'Source 的应用ID',
+CREATE TABLE `tb_tag` (
+  `id` bigint(20) NOT NULL COMMENT '标签ID',
   `userId` bigint(20) NOT NULL COMMENT '用户ID',
+  `appId` bigint(20) NOT NULL COMMENT '应用ID',
+  `tagType` bigint(20) DEFAULT '0' COMMENT 'tag的分类（比如是：知识、组织、人、图片）默认0',
+  `tagName` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT '标签名称',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `appId_userId_tagType_tagName` (`appId`,`userId`,`tagType`,`tagName`)
+) ENGINE=InnoDB AUTO_INCREMENT=20504 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='用户标签表';
+
+CREATE TABLE `tb_tag_source` (
+  `id` bigint(20) NOT NULL COMMENT '主键',
+  `tagId` bigint(20) NOT NULL COMMENT '标签ID',
+  `appId` bigint(20) NOT NULL COMMENT 'Source 的应用ID',
+  `userId` bigint(20) NOT NULL COMMENT '创建TagSource的人',
   `sourceId` bigint(20) NOT NULL COMMENT '资源ID',
-  `sourceType` int(20) NOT NULL COMMENT '资源类型 知识、人脉',
-  `sourceUrl` varchar(250) DEFAULT NULL COMMENT '自己地址',
-  `sourceTitle` varchar(100) DEFAULT NULL COMMENT '资源标题',
-  `sourceData` varchar(250) DEFAULT NULL COMMENT '资源的关键数据，备用',   
-  `invokeMethod` varchar(50) DEFAULT NULL COMMENT '资源可以使用的方法，备用',
+  `sourceType` bigint(20) DEFAULT 0  COMMENT '资源类型 知识、人脉',
   `createAt` bigint(20) DEFAULT '0' COMMENT '更新时间',
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `directoryId_appId_userId_sourceId_sourceType` (`directoryId`, `appId`,`userId`,`sourceId`,`sourceType`) 
-) ENGINE=InnoDB AUTO_INCREMENT=1307 DEFAULT CHARSET=utf8
-
-
+  UNIQUE KEY `tagId_appId_sourceId_sourceType` (`tagId`,`appId`,`sourceId`,`sourceType`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
