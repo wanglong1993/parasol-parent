e532e4dcc637c738195ba9d9f1ca5181a32a16f4
liurenyuan <liurenyuan@gintong.com>
2016-02-01 11:40:31 +0800

    Signed-off-by: liurenyuan <liurenyuan@gintong.com>

---
+++ parasol-user/parasol-user-web/src/main/java/com/ginkgocap/parasol/user/web/jetty/web/utils/Prompt.java
@@ -0,0 +1,64 @@
+package com.ginkgocap.parasol.user.web.jetty.web.utils;
+
+public final class Prompt {
+	public static final  String email_already_exists="邮箱已经存在";
+	public static final  String mobile_already_exists="手机号已经存在";
+	public static final  String email_format_is_not_correct="邮箱格式不正确";
+	public static final  String code_cannot_be_null_or_empty="验证码不能为空";
+	public static final  String code_is_not_right="验证码不正确";
+	public static final  String passowrd_cannot_be_null_or_empty="密码不能为空";
+	public static final  String password_length_must_be_greater_than_or_equal_to_6="密码长度须大于或等6位";
+	public static final  String confirmPassword_length_must_be_greater_than_or_equal_to_6="确认密码长度须大于或等6位";
+	public static final  String password_and_confirmPassword_do_not_match="密码和确认密码不一致";
+	public static final  String paramter_type_or_userType_error="参数type或userType的值不正确";
+	public static final  String userId_is_null_or_empty="用户id不能为空";
+	public static final  String passport_is_not_exists="手机或邮箱不存在";
+	public static final  String userId_is_null_or_empty_please_first_authentication_and_get_access_token="请先oauth认证获取用户access_token";
+	public static final  String passport_is_not_exists_in_UserLoginRegister="用户登录信息中不存在该手机号或邮箱";
+	public static final  String friendId_is_not_exists_in_UserLoginRegister="好友id在用户登录信息中不存在";
+	public static final  String has_been_add_this_friendly_please_waiting_for_him_to_agree="你已经添加该好友，请等待对方同意";
+	public static final  String he_s_already_a_good_friend_of_yours_cannot_repeat_add_him="该用户已经是你的好友，请不要重复添加";
+	public static final  String apply_to_add_friendly_successed="申请添加好友成功，等待对方审核同意";
+	public static final  String relationId_is_null_or_empty="申请好友的关系id不能为空";
+	public static final  String status_must_be_1="状态值必须为1";
+	public static final  String friendId_is_not_exists_in_UserBasic="好友id在用户基本信息中不存在";
+	public static final  String friendId_is_not_exists_in_UserOrganBasic="好友id在组织用户基本信息中不存在";
+	public static final  String update_Friendly_status_friendId_failed="审核通过好友申请时失败：";
+	public static final  String is_already_a_good_friend_of_yours_cannot_repeat_add_him=":已经是你的好友,请不要重复添加";
+	public static final  String delete_friendly_successed="删除好友成功";
+	public static final  String delete_friendly_failed="删除好友失败";
+	public static final  String userId_is_not_exist_in_UserOrganBasic="用户id在组织用户基本信息中不存在";
+	public static final  String updateUser_success="修改用户资料成功";
+	public static final  String userId_is_not_exist_in_UserBasic="用户id在用户基本信息中不存在";
+	public static final  String incorrect_password="密码不正确";
+	public static final  String get_access_token_is_null="获取access_token为空";
+	public static final  String get_access_token_failed="获取access_token失败";
+	public static final  String mobile_phone_number_is_not_correct="手机格式不正确";
+	public static final  String email_is_not_exists_in_UserLogniRegister="邮箱地址在用户登录信息中不存在";
+	public static final  String email_is_not_exists_in_UserBasic="邮箱地址在用户基本信息中不存在";
+	public static final  String email_is_not_exists_in_UserOrganBasic="邮箱地址在组织用户基本信息中不存在";
+	public static final  String email_has_expired="邮箱验证链接已经过期了";
+	public static final  String email_validate_success="邮箱验证成功了";
+	public static final  String newpassword_is_null_or_empty="新密码不能为空";
+	public static final  String oldpassword_is_null_or_empty="旧密码不能为空";
+	public static final  String newpassword_length_must_be_greater_than_or_equal_to_6="新密码必须大于或等于6位";
+	public static final  String oldpassword_length_must_be_greater_than_or_equal_to_6="旧密码必须大于或等于6位";
+	public static final  String incorrect_oldpassword="旧密码不正确";
+	public static final  String update_password_successed="修改密码成功";
+	public static final  String confirmPassword_is_null_or_empty="确认密码不能为空";
+	public static final  String passport_is_not_right_phone_number_or_email="邮箱地址或者手机号格式不正确";
+	public static final  String reset_password_successed="重置密码成功";
+	public static final  String reset_password_failed="重置密码失败";
+	public static final  String passport_is_null_or_empty="邮箱或者手机号不能为空";
+	public static final  String passport_already_exists="邮箱或者手机号已经存在";
+	public static final  String failed_to_get_the_mobile_verfication_code="获取手机验证码失败";
+	public static final  String sendmail_failed_get_the_verfication_code_failed="获取邮箱验证码失败";
+	public static final  String provinceId_is_null_or_empty="省id不能为空";
+	public static final  String start_must_be_than_zero="分页起始值必须大于0";
+	public static final  String count_must_be_than_zero="每页的数量不能小于0";
+	public static final  String not_found_userId_list="没有找到用户列表信息";
+	public static final  String thirdIndustryId_is_null_or_empty="第三级行业id不能为空";
+	
+	
+
+}
