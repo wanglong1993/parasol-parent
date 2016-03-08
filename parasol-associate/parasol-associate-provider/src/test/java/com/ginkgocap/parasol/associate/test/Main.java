86b52816f9f56f6876d21985bb6b2505dac506e3
allenshen <shenyonghua@jintong.com>
2016-03-08 11:49:21 +0800

    project init

---
+++ parasol-associate/parasol-associate-provider/src/test/java/com/ginkgocap/parasol/associate/test/Main.java
@@ -0,0 +1,17 @@
+package com.ginkgocap.parasol.associate.test;
+
+import java.util.Arrays;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+	public static void main(String[] args) {
+		String[] parentIds = StringUtils.split("1-2-3-4-5-6-7-8","-");
+		parentIds = Arrays.copyOfRange(parentIds, parentIds.length - 3, parentIds.length);
+		parentIds=new String[]{};
+		System.out.println(StringUtils.join(parentIds, "-"));
+		String cc = new String();
+		
+		System.out.println(cc.getClass().getPackage().getName());
+	}
+}
