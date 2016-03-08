d4a00dd935f97f3c802c88cff1bef028a4a123d4
allenshen <shenyonghua@jintong.com>
2016-03-08 11:49:22 +0800

    project init

---
+++ parasol-associate/parasol-associate-web/src/main/java/com/ginkgocap/parasol/associate/web/jetty/web/controller/BaseControl.java
@@ -0,0 +1,13 @@
+package com.ginkgocap.parasol.associate.web.jetty.web.controller;
+
+import org.apache.log4j.Logger;
+/**
+ * 
+ * @author allenshen
+ * @date 2015年11月23日
+ * @time 上午8:47:57
+ * @Copyright Copyright©2015 www.gintong.com
+ */
+public abstract class BaseControl {
+	private static Logger logger = Logger.getLogger(BaseControl.class);
+}
