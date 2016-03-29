185dad99ab487ced69e4615786e489fbd5b4e9d3
allenshen <shenyonghua@jintong.com>
2016-03-29 09:37:15 +0800

    init parasol-mapping

---
+++ parasol-common/parasol-mapping/parasol-mapping-api/src/test/java/com/ginkgocap/parasol/mapping/test/Main.java
@@ -0,0 +1,15 @@
+package com.ginkgocap.parasol.mapping.test;
+
+import com.ginkgocap.parasol.mapping.enumtype.MappingType;
+
+
+public class Main {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(MappingType.USER.value());
+		System.out.println(MappingType.KNOWLEDGE.value());
+
+	}
+}
