f44f466ae2c913298623d5882066620555f75d12
xutianlong <xutianlong@gintong.com>
2016-05-27 18:32:22 +0800

    UserBlackListController add delete

--- parasol-user/parasol-user-web/src/main/java/com/ginkgocap/parasol/user/web/jetty/web/controller/UserBlackListController.java
+++ parasol-user/parasol-user-web/src/main/java/com/ginkgocap/parasol/user/web/jetty/web/controller/UserBlackListController.java
@@ -1,6 +1,7 @@
 package com.ginkgocap.parasol.user.web.jetty.web.controller;
 
 import com.ginkgocap.parasol.oauth2.web.jetty.LoginUserContextHolder;
+import com.ginkgocap.parasol.user.exception.UserBlackListServiceException;
 import com.ginkgocap.parasol.user.model.UserBlackList;
 import com.ginkgocap.parasol.user.service.UserBlackListService;
 import com.ginkgocap.parasol.user.web.jetty.web.utils.Prompt;
@@ -31,14 +32,14 @@ public class UserBlackListController extends BaseControl{
     private UserBlackListService userBlackListService;
 
     /**
-     * 修改和创建感兴趣的标签
+     * 创建黑名单
      * @param  userBlackListId 需要添加到用户黑名单的用户id
      * @throws Exception
      * @return MappingJacksonValue
      */
     @RequestMapping(path = "/user/userblacklist/save", method = {RequestMethod.POST})
     public MappingJacksonValue saveUserBlackLsit(HttpServletRequest request, HttpServletResponse response
-        ,@RequestParam(name = "userBlackListId",required = true) Long userBlackListId)  {
+        ,@RequestParam(name = "userBlackListId",required = false) Long userBlackListId)  {
         Map<String, Object> resultMap = new HashMap<String, Object>();
         UserBlackList userBlackListEntary = new UserBlackList();
         Long userId = null;
@@ -50,7 +51,6 @@ public class UserBlackListController extends BaseControl{
                 resultMap.put("status",0);
                 return new MappingJacksonValue(resultMap);
             }
-
             appId = LoginUserContextHolder.getAppKey();
             if(ObjectUtils.isEmpty(appId)){
                 resultMap.put( "message", "appId不能为空！");
@@ -67,8 +67,44 @@ public class UserBlackListController extends BaseControl{
             resultMap.put("balcklsitId",balcklistId);
             return new MappingJacksonValue(resultMap);
         } catch(Exception e) {
-
+            resultMap.put("message", "创建黑名单失败！");
+            resultMap.put("status", 0);
+            return new MappingJacksonValue(resultMap);
         }
-        return null;
+    }
+
+    /**
+     * 根据Id删除黑名单
+     * @param  userblacklistId 需要添加到用户黑名单的用户id
+     * @throws Exception
+     * @return MappingJacksonValue
+     */
+    @RequestMapping(path = {"/user/userblacklist/delete"}, method = {RequestMethod.GET})
+    public MappingJacksonValue deleteUserBlackList(HttpServletRequest request, HttpServletResponse response
+            ,@RequestParam(name = "userblacklistId",required = false) Long userblacklistId) throws UserBlackListServiceException {
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        Long userId = null;
+        Long appId = null;
+        try {
+            userId = LoginUserContextHolder.getUserId();
+            if(userId==null){
+                resultMap.put("message", Prompt.userId_is_null_or_empty);
+                resultMap.put("status",0);
+                return new MappingJacksonValue(resultMap);
+            }
+            appId = LoginUserContextHolder.getAppKey();
+            if(ObjectUtils.isEmpty(appId)){
+                resultMap.put( "message", "appId不能为空！");
+                resultMap.put( "status", 0);
+                return new MappingJacksonValue(resultMap);
+            }
+            Boolean sgin = userBlackListService.delete(userblacklistId);
+            resultMap.put("message",sgin);
+            resultMap.put("status", 1);
+            return new MappingJacksonValue(resultMap);
+        } catch (Exception e) {
+            logger.error("删除黑名单失败！");
+            throw new UserBlackListServiceException("删除黑名单失败！");
+        }
     }
 }
