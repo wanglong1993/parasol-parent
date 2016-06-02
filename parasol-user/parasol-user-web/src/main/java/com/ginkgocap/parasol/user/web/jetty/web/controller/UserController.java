55ae5cf1637aba3a8e752101e863bf22857a2aa0
liurenyuan <liurenyuan@gintong.com>
2016-06-02 11:14:42 +0800

    Signed-off-by: liurenyuan <liurenyuan@gintong.com>

--- parasol-user/parasol-user-web/src/main/java/com/ginkgocap/parasol/user/web/jetty/web/controller/UserController.java
+++ parasol-user/parasol-user-web/src/main/java/com/ginkgocap/parasol/user/web/jetty/web/controller/UserController.java
@@ -3515,7 +3515,7 @@ public class UserController extends BaseControl {
 				if(isEmail(passport)){
 //					code=userLoginRegisterService.getIdentifyingCode(passport);
 //					if(StringUtils.isEmpty(code)){
-					if(emailtype!=0 && emailtype!=1 && emailtype!=1){
+					if(emailtype!=0 && emailtype!=1 && emailtype!=5){
 						resultMap.put( "message", Prompt.register_email_type_is_not_correcct);
 						resultMap.put( "status", 0);
 						return new MappingJacksonValue(resultMap);
