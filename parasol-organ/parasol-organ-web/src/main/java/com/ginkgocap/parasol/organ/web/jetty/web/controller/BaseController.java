c59a90d90e2a70f99956ddd44d55fc0a4aa90e8d
zhaobingbing <598892847@qq.com>
2016-06-28 15:53:55 +0800

    basecontroller用户写死

--- parasol-organ/parasol-organ-web/src/main/java/com/ginkgocap/parasol/organ/web/jetty/web/controller/BaseController.java
+++ parasol-organ/parasol-organ-web/src/main/java/com/ginkgocap/parasol/organ/web/jetty/web/controller/BaseController.java
@@ -144,21 +144,24 @@ public abstract class BaseController {
 
     protected User getUser(HttpServletRequest request) {
         // 判断客户端请求方式
-        if (isWebRequest(request)) {
-            String sessionId = request.getHeader("sessionID");
-            if (StringUtils.isNotBlank(sessionId)) {
-                String key = RedisKeyUtils.getSessionIdKey(sessionId);
-                return getUser(request, key);
-            }
-        } else {
-            String sessionId = request.getHeader("sessionID");
-            if (sessionId != null && !"null".equals(sessionId)
-                    && !"".equals(sessionId)) {
-                String key = "user" + sessionId;
-                return getUser(request, key);
-            }
-        }
-        return null;
+//        if (isWebRequest(request)) {
+//            String sessionId = request.getHeader("sessionID");
+//            if (StringUtils.isNotBlank(sessionId)) {
+//                String key = RedisKeyUtils.getSessionIdKey(sessionId);
+//                return getUser(request, key);
+//            }
+//        } else {
+//            String sessionId = request.getHeader("sessionID");
+//            if (sessionId != null && !"null".equals(sessionId)
+//                    && !"".equals(sessionId)) {
+//                String key = "user" + sessionId;
+//                return getUser(request, key);
+//            }
+//        }
+        User user = new User();
+        user.setId(1);
+        user.setName("yinxing");
+        return user;
     }
 
     private User getUser(HttpServletRequest request, String key) {
