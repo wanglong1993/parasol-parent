7e0c2537f8156a221a70f9748f29dd4810e1a4ba
Administrator <Administrator@john-PC>
2016-10-21 09:42:26 +0800

    Signed-off-by: Administrator <Administrator@john-PC>

--- parasol-user/parasol-user-web/src/main/java/com/ginkgocap/parasol/user/web/jetty/web/utils/Prompt.java
+++ parasol-user/parasol-user-web/src/main/java/com/ginkgocap/parasol/user/web/jetty/web/utils/Prompt.java
@@ -40,6 +40,7 @@ public final class Prompt {
 	public static final  String weixin_login_is_error="微信注册用户登录出错";
 	public static final  String get_access_token_failed="获取access_token失败";
 	public static final  String invild_code="code无效或者该code已经使用过";
+	public static final  String access_token_inviild="access_token未获取到";
 	public static final  String mobile_phone_number_is_not_correct="手机格式不正确";
 	public static final  String email_is_not_exists_in_UserLogniRegister="邮箱地址在用户登录信息中不存在";
 	public static final  String email_is_not_exists_in_UserBasic="邮箱地址在用户基本信息中不存在";
