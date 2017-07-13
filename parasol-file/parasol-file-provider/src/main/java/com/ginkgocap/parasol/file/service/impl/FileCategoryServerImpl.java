a281544eb705cf4bea15eb3cb8bd2011c3ae5bc8
xutlong126 <xutlong@126.com>
2017-07-13 10:53:47 +0800

    文件service

---
+++ parasol-file/parasol-file-provider/src/main/java/com/ginkgocap/parasol/file/service/impl/FileCategoryServerImpl.java
@@ -0,0 +1,13 @@
+package com.ginkgocap.parasol.file.service.impl;
+
+import com.ginkgocap.parasol.common.service.impl.BaseService;
+import com.ginkgocap.parasol.file.service.FileCategoryServer;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by xutlong on 2017/7/6.
+ */
+
+@Service("fileCategoryServerImpl")
+public class FileCategoryServerImpl extends BaseService implements FileCategoryServer {
+}
---
+++ parasol-file/parasol-file-provider/src/main/java/com/ginkgocap/parasol/file/service/impl/UserCategoryServerImpl.java
@@ -0,0 +1,12 @@
+package com.ginkgocap.parasol.file.service.impl;
+
+import com.ginkgocap.parasol.common.service.impl.BaseService;
+import com.ginkgocap.parasol.file.service.UserCategoryServer;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by xutlong on 2017/7/6.
+ */
+@Service("userCategoryServerImpl")
+public class UserCategoryServerImpl extends BaseService implements UserCategoryServer {
+}
