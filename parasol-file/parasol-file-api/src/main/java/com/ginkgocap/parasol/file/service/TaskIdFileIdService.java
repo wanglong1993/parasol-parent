0153832c057b5aff0266ac4d600f3414bee7f735
xutlong126 <xutlong@126.com>
2017-07-26 16:45:16 +0800

    taskId

---
+++ parasol-file/parasol-file-api/src/main/java/com/ginkgocap/parasol/file/service/TaskIdFileIdService.java
@@ -0,0 +1,20 @@
+package com.ginkgocap.parasol.file.service;
+
+import com.ginkgocap.parasol.file.model.TaskIdFileId;
+
+import java.util.List;
+
+public interface TaskIdFileIdService {
+
+    int insert(TaskIdFileId pojo);
+
+    int insertList(List< TaskIdFileId> pojos);
+
+    List<TaskIdFileId> select(TaskIdFileId pojo);
+
+    int update(TaskIdFileId pojo);
+
+    void deleteByTaskId(String taskId);
+
+    void deleteByTaskIdAndFileId(String taskId, String fileId);
+}
