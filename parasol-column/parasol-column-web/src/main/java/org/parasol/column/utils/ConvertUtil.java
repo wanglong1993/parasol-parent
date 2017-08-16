426d22506a7de2702145b5681e71cba0b53556b4
wangfei <wangfei01@gintong.com>
2017-08-16 16:09:45 +0800

    fix bug

---
+++ parasol-column/parasol-column-web/src/main/java/org/parasol/column/utils/ConvertUtil.java
@@ -0,0 +1,19 @@
+package org.parasol.column.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConvertUtil {
+    public static List<Long> convert(String demandIds) {
+        List<Long> ids = new ArrayList<Long>();
+        String[] r = demandIds.split(",");
+        if (r != null && r.length > 0) {
+            for (String id : r) {
+                if(!id.equals("")){
+                    ids.add(new Long(id));
+                }
+            }
+        }
+        return ids;
+    }
+}
